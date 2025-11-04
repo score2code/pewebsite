fc8f4f6a78dc5eded1453b823d0170fdd020e5cc
ui2code <hemerson.lourenco@gmail.com>
2025-11-04 03:42:59 +0000

    Deploying to deploy from @ score2code/pewebsite@90e06314524fa1f5f7cf02fc5df052d7204532f5 🚀

---
+++ _next/static/chunks/app/guias/mercados-over-under-gols/page-e4f9c0062bd599ab.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[730,409,569,870,223,390,784,682,268,864,982,109],{3521:function(){}},function(n){n.O(0,[971,117,744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
