80532d6e92a0429a729087f6eb8d32ee5bb54f05
ui2code <hemerson.lourenco@gmail.com>
2025-11-05 11:30:34 +0000

    Deploying to deploy from @ score2code/pewebsite@e2141f56ccc0aa0fa4380bf9cecdb2efe5e05406 🚀

---
+++ _next/static/chunks/app/guias/analise-de-confrontos-diretos/page-95fd4aece40dc5c6.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[910,160,907,687,156,743,392,508,230,842,456],{7502:function(n,e,u){Promise.resolve().then(u.t.bind(u,2972,23))}},function(n){n.O(0,[972,971,117,744],function(){return n(n.s=7502)}),_N_E=n.O()}]);
--- _next/static/chunks/app/guias/analise-de-confrontos-diretos/page-f4956e03dbf26f20.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[910,160,907,156,743,456,392,508,842,230],{7502:function(n,e,u){Promise.resolve().then(u.t.bind(u,2972,23))}},function(n){n.O(0,[972,971,117,744],function(){return n(n.s=7502)}),_N_E=n.O()}]);
