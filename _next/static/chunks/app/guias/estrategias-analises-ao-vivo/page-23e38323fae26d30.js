7e2e1d12838ba6d122b6edf94cc2e86d0a3f1bcf
ui2code <hemerson.lourenco@gmail.com>
2025-11-07 04:50:53 +0000

    Deploying to deploy from @ score2code/pewebsite@e1171734ce9dc0f3798912d1568baaeea51238ba 🚀

--- _next/static/chunks/app/guias/estrategias-analises-ao-vivo/page-23e38323fae26d30.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[6223,5330,7409,4789,749,4569,730,8268,4109,4982],{3521:function(){}},function(n){n.O(0,[2971,2117,1744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
