17c05bf303e690ddec6b7b44b5d39a47d5a28449
ui2code <hemerson.lourenco@gmail.com>
2025-11-07 07:30:52 +0000

    Deploying to deploy from @ score2code/pewebsite@debb9816a8e7cf067c4edd2a1f4e1b65b1d07ee6 🚀

--- _next/static/chunks/app/guias/o-que-e-handicap-asiatico/page-af93256469a20978.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[4109,7409,5330,4789,4569,749,6223,730,8268,4982],{3521:function(){}},function(n){n.O(0,[2971,2117,1744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
